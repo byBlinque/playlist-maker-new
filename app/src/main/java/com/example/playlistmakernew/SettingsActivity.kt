65d0d210cf2d85d587d9426237f246434344a266
Evgeniy Kim <asapwedo@gmail.com>
2023-02-13 15:34:43 +1000

    Support and agreement intents added

--- app/src/main/java/com/example/playlistmakernew/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakernew/SettingsActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat.startActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +18,24 @@ class SettingsActivity : AppCompatActivity() {
         }
         val settingsAcShare: RelativeLayout = findViewById(R.id.settings_ac_share)
         settingsAcShare.setOnClickListener {
-            val textToSend: String = "https://practicum.yandex.ru/learn/android-developer"
             val intent: Intent = Intent()
             intent.action = Intent.ACTION_SEND
             intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT, textToSend)
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_share_intent_text))
+            startActivity(intent)
+        }
+        val settingsAcSupport: RelativeLayout = findViewById(R.id.settings_ac_support)
+        settingsAcSupport.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:" + getString(R.string.settings_support_intent_email_adress))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.settings_support_intent_email_subject))
+            intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.settings_support_intent_email_text))
+            startActivity(intent)
+        }
+        val settingsAcAgreement: RelativeLayout = findViewById(R.id.settings_ac_agreement)
+        settingsAcAgreement.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setData(Uri.parse(getString(R.string.settings_agreement)))
             startActivity(intent)
         }
     }
