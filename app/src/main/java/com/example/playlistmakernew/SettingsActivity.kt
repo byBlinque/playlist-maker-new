ae5ce97c781d6a337daf1ebefdc151dc9cd48bb6
Evgeniy Kim <asapwedo@gmail.com>
2023-02-13 15:08:13 +1000

    Share Intent added

--- app/src/main/java/com/example/playlistmakernew/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmakernew/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.playlistmakernew
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.RelativeLayout
@@ -19,7 +20,9 @@ class SettingsActivity : AppCompatActivity() {
         val settingsAcShare: RelativeLayout = findViewById(R.id.settings_ac_share)
         settingsAcShare.setOnClickListener {
             val textToSend: String = "https://practicum.yandex.ru/learn/android-developer"
-            val intent: Intent = Intent(Intent.ACTION_SEND)
+            val intent: Intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.type = "text/plain"
             intent.putExtra(Intent.EXTRA_TEXT, textToSend)
             startActivity(intent)
         }
