a52cb8fa85d2466876d5d92537ef2ac972451bdd
Evgeniy Kim <asapwedo@gmail.com>
2023-02-15 14:41:59 +1000

    Search activity xml elements and logic added

--- app/src/main/java/com/example/playlistmakernew/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakernew/SearchActivity.kt
@@ -2,10 +2,47 @@ package com.example.playlistmakernew
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val searchET = findViewById<EditText>(R.id.search_et)
+        val clearButton = findViewById<Button>(R.id.clear_btn)
+
+        clearButton.setOnClickListener {
+            searchET.setText("")
+        }
+
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        }
+        searchET.addTextChangedListener(textWatcher)
     }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
 }
