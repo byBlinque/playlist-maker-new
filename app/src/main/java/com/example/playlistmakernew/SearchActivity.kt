a42ac0a60c939b672602ea30b41b5d1e4b11d577
Evgeniy Kim <asapwedo@gmail.com>
2023-02-17 14:47:21 +1000

    onSavedInstanceState() & onRestoreInstanceState() commited

--- app/src/main/java/com/example/playlistmakernew/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmakernew/SearchActivity.kt
@@ -10,15 +10,25 @@ import android.widget.EditText
 import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val SEARCH_EDIT_TEXT = "SEARCH_EDIT_TEXT"
+    }
+    val searchET: EditText = findViewById<EditText>(R.id.search_et)
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
-        val searchET = findViewById<EditText>(R.id.search_et)
+        //searchET = findViewById<EditText>(R.id.search_et)
         val clearButton = findViewById<ImageView>(R.id.clear_btn)
+        val backButton = findViewById<ImageView>(R.id.back_btn)
 
         clearButton.setOnClickListener {
             searchET.setText("")
+        }
+
+        backButton.setOnClickListener {
+            finish()
         }
 
         val textWatcher = object : TextWatcher {
@@ -37,6 +47,16 @@ class SearchActivity : AppCompatActivity() {
         searchET.addTextChangedListener(textWatcher)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(SEARCH_EDIT_TEXT, searchET.text.toString())
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        searchET.setText(savedInstanceState.getString(SEARCH_EDIT_TEXT,""))
+        super.onRestoreInstanceState(savedInstanceState)
+    }
+
     private fun clearButtonVisibility(s: CharSequence?): Int {
         return if (s.isNullOrEmpty()) {
             View.GONE
@@ -44,5 +64,7 @@ class SearchActivity : AppCompatActivity() {
             View.VISIBLE
         }
     }
+
+
 
 }
